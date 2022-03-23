4a2d26baeb220e5f1fa24fb28932f7358ede62a5
Vinson Sheep <775014077@qq.com>
2022-03-23 15:23:55 +0800

    reboot.py: detect training_node crashs.

--- test/reboot.py
+++ test/reboot.py
@@ -4,9 +4,11 @@
 from os import system
 import rospy
 from sensor_msgs.msg import LaserScan
+from multi_rotor_avoidance_rl.msg import Acc
 import time
 
 last_time = time.time()
+last_time_acc = time.time()
 reboot_flag = False
 episode = 0
 
@@ -14,13 +16,18 @@ def scanCB(msg):
     global last_time
     last_time = time.time()
 
+def accCB(msg):
+    global last_time_acc
+    last_time_acc = time.time()
+
 if __name__ == '__main__':
 
     rospy.init_node("reboot_node")
     scanSub = rospy.Subscriber("iris/scan", LaserScan, scanCB)
+    accSub = rospy.Subscriber("/accel_body", Acc, accCB)
 
     while True:
-        if (time.time() - last_time) > 20.0:
+        if (time.time() - last_time) > 20.0 or (time.time() - last_time_acc) > 20:
             if reboot_flag == False:
                 print("Reboot in 10s.")
                 system("shutdown -r -t 10")
